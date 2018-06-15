fbcba4dac32f950fbe6a309ead8f999ede23ea99
Bruno Procopio <brunoprocopio596@gmail.com>
2018-06-15 18:46:07 -0300

    Update python template

--- python/template.py
+++ python/template.py
@@ -1,12 +1,22 @@
 import time
 
 
+runs = 1000
+
+
 def main():
-    pass
+    return 0
 
 
 if __name__ == "__main__":
-    start_time = time.time()
-    main()
-    total_time = (time.time() - start_time) * 1000
-    print("runtime --- {:.4f} ms ---".format(total_time))
+    answer = 0
+    total_time = 0
+
+    for run in range(0, runs):
+        start_time = time.time()
+        answer = main()
+        run_time = time.time() - start_time
+        total_time += run_time
+
+    print("answer  --- {} ---".format(answer))
+    print("runtime --- {:.4f} ms ---".format((total_time * 1000) / runs))

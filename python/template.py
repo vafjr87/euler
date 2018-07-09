181a267da4076d62d291d62e6314db548bbbc68f
Virgilio Fernandes Jr <vafjr87@vafjr87mbp.local>
2018-07-09 01:33:29 -0300

    Resolução do problema 08

--- python/template.py
+++ python/template.py
@@ -1,7 +1,6 @@
 import time
 
 
-runs = 1000
 
 
 def main():
@@ -12,11 +11,9 @@ if __name__ == "__main__":
     answer = 0
     total_time = 0
 
-    for run in range(0, runs):
-        start_time = time.time()
-        answer = main()
-        run_time = time.time() - start_time
-        total_time += run_time
+    start_time = time.time()
+    answer = main()
+    run_time = time.time() - start_time
 
     print("answer  --- {} ---".format(answer))
-    print("runtime --- {:.4f} ms ---".format((total_time * 1000) / runs))
+    print("runtime --- {:.4f} ms ---".format(run_time)
